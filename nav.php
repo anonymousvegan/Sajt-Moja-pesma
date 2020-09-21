eb503fcab1d67e7cac0dc5fddb3e4766512488bb
nikola <majjunjulavgust@gmail.com>
2020-09-21 22:56:24 +0200

    eeeeee sad oće

--- nav.php
+++ nav.php
@@ -1,21 +1,14 @@
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark editInCss">
-        <a class="navbar-brand" href="#">
-          <img src="fajlovi/logo.png" id="logo" class="d-inline-block align-top" alt="">
-          Anonimna Rec
-        </a>
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Početna</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" onclick="dodaj()" class="nav-link">Dodaj tekst</a>
-                </li>
-            </ul>
-        </div>
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Pretraži" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pretraži</button>
-        </form>
-    </nav>
+    <header>
+        <h1 class="logo">Anonimna reč</h1>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle">
+        <nav>
+          <ul>
+            <li><a href="#">Početna</a></li>
+            <li><a href="#">Dodaj tekst</a></li>
+          </ul>  
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label">
+          <span></span>
+        </label>
+    </header>
