14257698e08435e63f54debd167888813511b090
nikola <majjunjulavgust@gmail.com>
2020-10-06 17:17:28 +0200

    zeza git

--- pesma-preko-celog-ekrana.php
+++ pesma-preko-celog-ekrana.php
@@ -5,10 +5,11 @@
     <p id="tekst-pesme-preko-celog-ekrana"></p>
     </div>
     <div class="okvir-za-komentare">
-    <form method="post" action="dodajkomentar.php">
-    <input type="text" name="komentar">
-    <button type="submit">komentarisi</button>
-    </form>
+    <form method="post" action="backend/pozadinske/dodajkomentar.php">
+    <input type="hidden" name="autor" value="<?php echo $_SESSION['ime']?>">
+    <input id="id-pesme-za-komentarisanje" type="hidden" name="idpesme">
+    <input  value="help pls" id="unos-komentara" type="text" name="komentar">
+    <button type="submit">unesikomet</button>
     </div>
     <div class="x" onclick="zatvori_pesmu_preko_celog_ekrana()">&times;</div>
 </div>
