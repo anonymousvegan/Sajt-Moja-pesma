dbfe38f59fd07bd18f005102878ae9599d81ff1b
nikola <majjunjulavgust@gmail.com>
2020-11-02 23:43:10 +0100

    responsiv uredi profil

--- backend/pozadinske/promeni_informacije.php
+++ backend/pozadinske/promeni_informacije.php
@@ -9,18 +9,18 @@ if(isset($_SESSION["ime"])){
         $email = $_POST["email"];
         $godine = $_POST["godine"];
         $biografija = $_POST["biografija"];
-        if(empty($korisnicko_ime) || empty($email) || empty($godine)) {
-            header("location: ../../uredi-profil.php?greska=prazno-polje");
-            exit();
-        }
+            if(empty($korisnicko_ime) || empty($email) || empty($godine)) {
+                header("location: ../../uredi-profil.php?greska=prazno-polje");
+                exit();
+            }
         if (isset($ime)){
-            if (!preg_match("/^[a-zA-Z0-9šđćčžŠĐĆČŽабвгдђежзијклљмнљопрстћуфхцчџшАБВГДЂЕЖЗИЈКЛЉМНЊОПРИСТЋУФХЦЧЏШ]*$/", $ime)){
+            if (!preg_match("/^[a-zA-ZšđćčžŠĐĆČŽабвгдђежзијклљмнљопрстћуфхцчџшАБВГДЂЕЖЗИЈКЛЉМНЊОПРИСТЋУФХЦЧЏШ]*$/", $ime)){
                 header("location: ../../uredi-profil.php?greska=neispravno-ime");
                 exit();
             }
         }
-        if (isset($ime)){
-            if (!preg_match("/^[a-zA-Z0-9šđćčžŠĐĆČŽабвгдђежзијклљмнљопрстћуфхцчџшАБВГДЂЕЖЗИЈКЛЉМНЊОПРИСТЋУФХЦЧЏШ]*$/", $prezime)){
+        if (isset($prezime)){
+            if (!preg_match("/^[a-zA-ZšđćčžŠĐĆČŽабвгдђежзијклљмнљопрстћуфхцчџшАБВГДЂЕЖЗИЈКЛЉМНЊОПРИСТЋУФХЦЧЏШ]*$/", $prezime)){
                 header("location: ../../uredi-profil.php?greska=neispravo-prezime");
                 exit();
             }
@@ -36,7 +36,7 @@ if(isset($_SESSION["ime"])){
             exit();
             }
         }
-        if(!filter_var("some@address.com", FILTER_VALIDATE_EMAIL)){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
             header("location: ../../uredi-profil.php?greska=nesipravan-email");
             exit();
         }
@@ -52,6 +52,8 @@ if(isset($_SESSION["ime"])){
                 mysqli_stmt_execute($stmt);
                 $rezultati=mysqli_stmt_get_result($stmt);
                 if($red=mysqli_fetch_assoc($rezultati)){
+                $staro_ime=$red["ime"];
+                $stari_email=$red["email"];
                     if($red["ime"]==$korisnicko_ime && $red["email"]==$email){
                         $sql= "UPDATE korisnici set pravoime=?, prezime=?, godine=?, biografija=? WHERE id=?;";
                         $stmt=mysqli_stmt_init($conn);
@@ -65,53 +67,118 @@ if(isset($_SESSION["ime"])){
                             header("location: ../../uredi-profil.php?uspeh=informacije-promenjene");
                         }  
                     }
-                    else{
-                        $sql = "SELECT  ime FROM korisnici where ime=?;";
-                        $stmt= mysqli_stmt_init($conn); 
+                    #ako menja samo email
+                    else if($staro_ime==$korisnicko_ime && $stari_email!=$email){
+                        $sql= "SELECT * FROM korisnici WHERE email=?";
+                        $stmt=mysqli_stmt_init($conn);
                         if(!mysqli_stmt_prepare($stmt, $sql)){
                             header("location: ../../uredi-profil.php?greska=sqlgreksa4");
                             exit();
                         }
                         else{
-                            mysqli_stmt_bind_param($stmt, "s", $korisnicko_ime);
+                            mysqli_stmt_bind_param($stmt, s, $email);
                             mysqli_stmt_execute($stmt);
-                            mysqli_stmt_store_result($stmt);
-                            $proverarezultata=mysqli_stmt_num_rows($stmt);
-                            if($proverarezultata>0){
-                                header("location: ../../uredi-profil.php?greska=ime_zauzeto");
-                                exit();  
+                            $rezultati=mysqli_stmt_get_result($stmt);
+                            $broj= mysqli_num_rows($rezultati);
+                            if($broj>0){
+                                header("location: ../../uredi-profil.php?greska=email_zauzet");
+                                exit();
                             }
                             else{
-                                $sql = "SELECT  email FROM korisnici where email=?;"; 
-                                $stmt= mysqli_stmt_init($conn); 
+                                $sql= "UPDATE korisnici set pravoime=?, prezime=?, godine=?, biografija=?, email=? WHERE id=?;";
+                                $stmt=mysqli_stmt_init($conn);
                                 if(!mysqli_stmt_prepare($stmt, $sql)){
-                                    header("location: ../../uredi-profil.php?greska=sqlgreksa5");
+                                    header("location: ../../uredi-profil.php?greska=sqlgreska5");
                                     exit();
                                 }
                                 else{
-                                    mysqli_stmt_bind_param($stmt, "s", $email);
+                                    mysqli_stmt_bind_param($stmt, "ssssss", $ime, $prezime, $godine, $biografija, $email, $_SESSION["id"]);
                                     mysqli_stmt_execute($stmt);
-                                    mysqli_stmt_store_result($stmt);
-                                    $proverarezultata=mysqli_stmt_num_rows($stmt);
-                                    if($proverarezultata>0){
-                                        header("location: ../../uredi-profil.php?greska=email_zauzet");
-                                        exit();  
+                                    header("location: ../../uredi-profil.php?uspeh=informacije-promenjene");
+                                }
+                            }
+                        }
+                    }
+                    #ako menja samo ime
+                    else if($staro_ime!=$korisnicko_ime && $stari_email==$email){
+                        $sql= "SELECT * FROM korisnici WHERE ime=?";
+                        $stmt=mysqli_stmt_init($conn);
+                        if(!mysqli_stmt_prepare($stmt, $sql)){
+                            header("location: ../../uredi-profil.php?greska=sqlgreksa6");
+                            exit();
+                        }
+                        else{
+                            mysqli_stmt_bind_param($stmt, s, $korisnicko_ime);
+                            mysqli_stmt_execute($stmt);
+                            $rezultati=mysqli_stmt_get_result($stmt);
+                            $broj= mysqli_num_rows($rezultati);
+                            if($broj>0){
+                                header("location: ../../uredi-profil.php?greska=ime_zauzeto");
+                                exit();
+                            }
+                            else{
+                                $sql= "UPDATE korisnici set pravoime=?, prezime=?, godine=?, biografija=?, ime=? WHERE id=?;";
+                                $stmt=mysqli_stmt_init($conn);
+                                if(!mysqli_stmt_prepare($stmt, $sql)){
+                                    header("location: ../../uredi-profil.php?greska=sqlgreska5");
+                                    exit();
+                                }
+                                else{
+                                    mysqli_stmt_bind_param($stmt, "ssssss", $ime, $prezime, $godine, $biografija, $korisnicko_ime, $_SESSION["id"]);
+                                    mysqli_stmt_execute($stmt);
+                                    header("location: ../../uredi-profil.php?uspeh=informacije-promenjene");
+                                }
+                            }
+                        }
+                    }
+                    #u slučaju da se menjaju i email i  username
+                    else if($staro_ime!=$korisnicko_ime && $stari_email!=$email){
+                        $sql= "SELECT * FROM korisnici WHERE email=?";
+                        $stmt=mysqli_stmt_init($conn);
+                        if(!mysqli_stmt_prepare($stmt, $sql)){
+                            header("location: ../../uredi-profil.php?greska=sqlgreksa7");
+                            exit();
+                        }
+                        else{
+                            mysqli_stmt_bind_param($stmt, s, $email);
+                            mysqli_stmt_execute($stmt);
+                            $rezultati=mysqli_stmt_get_result($stmt);
+                            $broj= mysqli_num_rows($rezultati);
+                            if($broj>0){
+                                header("location: ../../uredi-profil.php?greska=email_zauzet");
+                                exit();
+                            }
+                            else{
+                                $sql= "SELECT * FROM korisnici WHERE ime=?";
+                                $stmt=mysqli_stmt_init($conn);
+                                if(!mysqli_stmt_prepare($stmt, $sql)){
+                                    header("location: ../../uredi-profil.php?greska=sqlgreksa8");
+                                    exit();
+                                }
+                                else{
+                                    mysqli_stmt_bind_param($stmt, s, $korisnicko_ime);
+                                    mysqli_stmt_execute($stmt);
+                                    $rezultati=mysqli_stmt_get_result($stmt);
+                                    $broj= mysqli_num_rows($rezultati);
+                                    if($broj>0){
+                                        header("location: ../../uredi-profil.php?greska=ime_zauzeto");
+                                        exit();
                                     }
                                     else{
-                                        $sql= "UPDATE korisnici set ime=?, email=?,  pravoime=?, prezime=?, godine=?, biografija=? WHERE id=?;";
+                                        $sql= "UPDATE korisnici set pravoime=?, prezime=?, godine=?, biografija=?, ime=?, email=? WHERE id=?;";
                                         $stmt=mysqli_stmt_init($conn);
                                         if(!mysqli_stmt_prepare($stmt, $sql)){
-                                            header("location: ../../uredi-profil.php?greska=sqlgreksa2");
+                                            header("location: ../../uredi-profil.php?greska=sqlgreska9");
                                             exit();
                                         }
                                         else{
-                                            mysqli_stmt_bind_param($stmt, "sssssss", $korisnicko_ime, $email, $ime, $prezime, $godine, $biografija, $_SESSION["id"]);
+                                            mysqli_stmt_bind_param($stmt, "sssssss", $ime, $prezime, $godine, $biografija, $korisnicko_ime, $email, $_SESSION["id"]);
                                             mysqli_stmt_execute($stmt);
                                             header("location: ../../uredi-profil.php?uspeh=informacije-promenjene");
                                         }
                                     }
                                 }
-                            }
+                            }       
                         }
                     }
                 }
